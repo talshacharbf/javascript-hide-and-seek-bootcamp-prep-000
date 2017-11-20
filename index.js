51e408f9186c15d2b7df6c095cae6c06b8225287
tal shachar <tal.shachar@buzzfeed.com>
2017-11-20 00:22:29 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,32 @@
+function getFirstSelector (selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested .target');
+}
+
+function increaseRankBy(n) {
+var rank = document.getElementsByClassName('ranked-list')
+for (let i = 0; i < rank.length; i++) {
+  rank[i].innerHTML = parseInt()+ n
+}
+  }
+
+function deepestChild() {
+  var current = document.querySelector(`div#grand-node`)
+  var next = []
+  while (current) {
+    if (querySelector(current) === false) {
+      return current
+    }
+    if (current.isarray(current)) {
+      for (let i =0; i < currentl.length; i++) {
+        next.push(current[i])
+      }
+    }
+current = next.shift()
+  }
+  return null
+
+}
